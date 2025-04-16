253b130ba00ffc2a62f6ea85d9a9f0a69963a76b
wiktoria-coder <u64827@student.wat.edu.pl>
2025-04-16 19:04:58 +0200

    wip: Add interactive map

--- utils/model.py
+++ utils/model.py
@@ -1,7 +1,11 @@
-users:list = [
-    {"name":"Wiktoria","location":"Chełm","posts":100},
-    {"name":"Mateusz","location":"Węgorzewo","posts":6},
-    {"name":"Sabina","location":"Opole","posts":110},
-    {"name":"Weronika","location":"Tomaszów Mazowiecki","posts":300},
-    {"name":"Julia","location":"Zyrardów","posts":50},
-]
+users: list = [
+    {"name": "Wiktoria", "location": "Chełm", "posts": 100,
+     'picture': 'https://www.wig.wat.edu.pl/images/aktualnosci/2025/sklad_kpn_700.jpg'},
+    {"name": "Mateusz", "location": "Węgorzewo", "posts": 6,
+     'picture': 'https://www.wig.wat.edu.pl/images/aktualnosci/2025/sklad_kpn_700.jpg'},
+    {"name": "Sabina", "location": "Opole", "posts": 110,
+     'picture': 'https://www.wig.wat.edu.pl/images/aktualnosci/2025/sklad_kpn_700.jpg'},
+    {"name": "Weronika", "location": "Tomaszów Mazowiecki", "posts": 300,
+     'picture': 'https://www.wig.wat.edu.pl/images/aktualnosci/2025/sklad_kpn_700.jpg'},
+    {"name": "Julia", "location": "Żyrardów", "posts": 50,
+     'picture': 'https://www.wig.wat.edu.pl/images/aktualnosci/2025/sklad_kpn_700.jpg'},]
