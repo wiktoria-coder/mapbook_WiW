58f0e391f74c19cc3377ec2d2ba5f5d033165f85
wiktoria-coder <u64827@student.wat.edu.pl>
2025-04-16 16:52:50 +0200

    wip: Add update user functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils.model import users
-from utils.controller import get_user_info, add_user, remove_user
+from utils.controller import get_user_info, add_user, remove_user, update_user
 
 
 def main():
@@ -11,12 +11,14 @@ def main():
         print("1 - Pokaż co u moich znajomych")
         print("2 - Dodaj znajomego")
         print("3 - Usuń znajomego")
+        print("4 - Edytuj dane znajomego")
         print("=============================")
         choice: str = input("Wybierz opcję menu: ")
         if choice == "0": break
         if choice == "1": get_user_info(users[1:])
         if choice == "2": add_user(users)
         if choice == "3": remove_user(users)
+        if choice == "4": update_user(users)
 
 
 if __name__ == "__main__":
