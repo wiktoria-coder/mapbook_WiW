36de00ea6a8d250687821254fc8a27c0c3da6494
wiktoria-coder <u64827@student.wat.edu.pl>
2025-04-16 16:39:00 +0200

    wip: Add remove user functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils.model import users
-from utils.controller import get_user_info, add_user
+from utils.controller import get_user_info, add_user, remove_user
+
 
 def main():
     print(f"Witaj {users[0]["name"]}")
@@ -9,12 +10,13 @@ def main():
         print("0 - Zakończ pracę programu")
         print("1 - Pokaż co u moich znajomych")
         print("2 - Dodaj znajomego")
+        print("3 - Usuń znajomego")
         print("=============================")
-        choice:str=input("Wybierz opcję menu: ")
+        choice: str = input("Wybierz opcję menu: ")
         if choice == "0": break
         if choice == "1": get_user_info(users[1:])
         if choice == "2": add_user(users)
-
+        if choice == "3": remove_user(users)
 
 
 if __name__ == "__main__":
--- utils/controller.py
+++ utils/controller.py
@@ -1,9 +1,17 @@
-def get_user_info(users_data:list)->None:
+def get_user_info(users_data: list) -> None:
     for user in users_data:
         print(f"Twój znajomy {user["name"]} z {user["location"]} opublikował {user["posts"]} postów.")
 
-def add_user(users_data: list)->None:
+
+def add_user(users_data: list) -> None:
     new_name = input("Podaj imię: ")
     new_location = input("Podaj miejsce: ")
     new_posts = input("Podaj liczbę postów: ")
-    users_data.append({"name":new_name,"location": new_location,"posts": new_posts})
+    users_data.append({"name": new_name, "location": new_location, "posts": new_posts})
+
+
+def remove_user(users_data: list[dict]) -> None:
+    user_name = input("Podaj imię znajomego do usuniecia: ")
+    for user in users_data:
+        if user["name"] == user_name:
+            users_data.remove(user)
